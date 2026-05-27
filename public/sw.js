8a48b0fed1d9c2700e72a7e550ecdec736a81284
AUNG THU HTWE <ath.softwareengineering@gmail.com>
2026-05-27 13:54:01 +0630

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,45 @@
 const CACHE_NAME = 'ath-hub-v1';
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
+// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Cache opened');
         return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Cache addAll error:', err);
       })
   );
 });
 
+// Fetch with network first, fallback to cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; 
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
